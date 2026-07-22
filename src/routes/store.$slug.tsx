1510df06ceee4822dd71a6ded1c5124e260616f1
AlaminMX <164647907+AlaminMX@users.noreply.github.com>
2026-07-22 09:00:55 +0100

    Add VerifiedBadge component for verified sellers

--- src/routes/store.$slug.tsx
+++ src/routes/store.$slug.tsx
@@ -20,6 +20,7 @@ import { Footer } from "@/components/Footer";
 import { ProductCard } from "@/components/ProductCard";
 import { BackButton } from "@/components/BackButton";
 import { VerificationBanner, ApprovedBanner } from "@/components/VerificationBanner";
+import { VerifiedBadge } from "@/components/VerifiedBadge";
 import { ImageUploader } from "@/components/ImageUploader";
 import { SectionLoader } from "@/components/LoadingSpinner";
 import {
@@ -499,10 +500,9 @@ function StorePage() {
                   <TooltipProvider>
                     <Tooltip>
                       <TooltipTrigger asChild>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                          className="mt-1.5 h-6 w-6 cursor-default text-amber-500" aria-label="Verified seller">
-                          <path fillRule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
-                        </svg>
+                        <span className="mt-1.5 cursor-default">
+                          <VerifiedBadge className="h-6 w-6" />
+                        </span>
                       </TooltipTrigger>
                       <TooltipContent>Vouched for by {localVouchCount ?? 0}+ trusted sellers</TooltipContent>
                     </Tooltip>
